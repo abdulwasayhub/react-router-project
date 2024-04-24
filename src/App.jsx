c18978a49edb33a52d7e6bf9840cc621b9e8d839
Abdul Wasay <munirabdulwasay@gmail.com>
2024-04-24 20:37:20 +0500

    react router

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Home from "./components/Home/Home";
+import Footer from "./components/Header/Footer/Footer";
+import Header from "./components/Header/Header";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <></>;
+}
+
+export default App;
